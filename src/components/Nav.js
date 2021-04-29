375badce2b2edf54e163754073d6d3ae39229378
Andrew Cruz <andrewcruz@Mac-mini.local>
2021-04-29 03:47:42 -0400

    add photos folder and upload photos

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,13 +10,13 @@ const Nav = () => {
             <nav>
                 <div class="nav-wrapper">
                     <a><Link class="brand-logo center">Logo</Link></a>
-                    <a><Link data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></Link></a>
+                    {/* <a><Link data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></Link></a> */}
                 </div>
             </nav>
-            <ul id="slide-out" class="sidenav">
+            {/* <ul id="slide-out" class="sidenav">
                 <li><a href="#"><i class="material-icons">cloud</i>First Link With Icon</a></li>
                 <li><a href="#">Second Link</a></li>
-            </ul>
+            </ul> */}
         </body>
 
     )
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,7 +4,7 @@ import Nav from '../components/Nav'
 export default () => {
     return (
         <div>
-            <Nav />
+            
             <h1> We Are Running</h1>
         </div>
     )
