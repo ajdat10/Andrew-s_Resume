16a8b386141fa9d77ce6f0a6a520c03176613214
Andrew Cruz <andrewcruz@Mac-mini.local>
2021-04-29 05:04:49 -0400

    add photo with card onto intro page

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Nav from '../components/Nav'
+import Pic from '../photos/ProfessionalPic.jpg'
+import '../styles/App.css'
 
 export default () => {
     return (
         <div>
-            
-            <h1> We Are Running</h1>
+            <div class="row">
+                <div class="col">
+                    <div class="card-panel teal">
+                        <h1> We Are Running</h1>
+                        <img id="mainPic" src={Pic} alt='image of man' />
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
