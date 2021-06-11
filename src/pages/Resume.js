da6bb275b4a71a53f266c0d671b56b0f21059e69
Andrew Cruz <71989360+ajdat10@users.noreply.github.com>
2021-06-10 20:04:28 -0400

    adding side info

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -12,10 +12,26 @@ export default () => {
         <div class='profile-nav-info'>
           <h3 class='user-name'>Andrew J Cruz</h3>
         </div>
+        <div>
+          <p class="job-title">Software Engineer</p>
+        </div>
         <div class='address'>
-          <p class="state">New York,</p>
+          <p class="city/state">Brooklyn, NY</p>
+        </div>
+        <div class="main-bd">
+          <div class="left-side">
+            <p class="modile-number">
+              <i class="fa fa-phone"> (718)-844-1817</i>
+            </p>
+            <div class="user-email">
+              <i class="fa fa-envelope"> Andrew.j.cruz10@gmail.com</i>
+            </div>
+          </div>
 
         </div>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
       </div>
     </div>
   )
