c7b902dbe80db666e368a925b4b808e97dc2ed3b
Andrew Cruz <ajdat10@gmail.com>
2021-06-16 02:55:28 -0400

    added all work experience

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -49,19 +49,44 @@ export default () => {
               <div class='profile-experience tab'>
                 <h1>EXPERIENCE</h1>
                 <ul>
-                  <li>
-                    <h6>Software Engineer</h6>
-                    <h7>General Assembly</h7>
+                  <li class='engineer'>
+                    <h5>Software Engineer Oct 2020 - Jan 2021</h5>
+                    <h6>General Assembly</h6>
                     <li>•	Developed and design over three innovative web applications with React written on Visual Studio </li>
                     <li>•	Operated on MacOS’s terminal where NPM, GIT, Bash, ZSH, and Homebrew was used</li>
                     <li>•	Worked on different languages, such as JavaScript and Python</li>
-                    <li></li>
-                    <li></li>
+                    <li>•	Analyzed and resolved technical and web application problems</li>
+                    <li>•	Participated in code reviews, to grow and contribute to the growth around my teamwork cross-functionally to build new features and problem solving</li>
+                    <li>•	Completed in-class projects, hackathons, coding challenges, databases challenges, team collaborations, debugging, and personal projects focused on real-world applications of software engineering principles and best practices</li>
+                  </li>
+                  <li class='doorman'>
+                    <h5>Doorman/Porter Jun 2020 - Present</h5>
+                    <h6>Silver Towers</h6>
+                    <li>•	Ensure building security and safety for tenants to include guest registration; check identification, call to the tenant to announce the person(s) who is visiting/delivering, etc.</li>
+                    <li>•	Answer the lobby phone and respond to calls accordingly</li>
+                    <li>•	Perform all assigned work to ensure the safety of the building's tenants and the continuous operation of the site</li>
+                    <li>•	Emptied and cleaned waste receptacles, Vacuumed rugs and carpeted areas, Polished all glass and minor building maintenance and repairs</li>
+                  </li>
+                  <li class='store'>
+                    <h5>Store Manager Oct 2019 - Dec 2019</h5>
+                    <h6>CVS Health</h6>
+                    <li>•	Manage all retail in-store daily operations, verifying all incoming/outgoing cash transaction</li>
+                    <li>•	Learn daily operations in the pharmacy department verifying prescriptions, account then validate specific orders, and customer service</li>
+                    <li>•	Responsible for making sure the store is fully stock with proper signage not limited to pricing, sales and promotions</li>
+                    <li>•	Manage all incoming shipments and maintain stock room organization</li>
+                  </li>
+                  <li class='hr'>
+                    <h5>Human Resources Manager Oct 2017 - Oct 2019</h5>
+                    <h6>BJ's Wholesale Club</h6>
+                    <li>•	Partnered with the Field Recruiter to identify store-wide staffing needs</li>
+                    <li>•	Routinely coordinated with other club managers to fulfill business needs in scheduling and training </li>
+                    <li>•	Managed all employee payroll, processed benefits, worker's compensation, etc.</li>
+                    <li>•	Insured BJ's comply with all labor laws that govern New York</li>
                   </li>
                 </ul>
               </div>
               <div class='profile-education tab'>
-
+                <h1>EDUCATION</h1>
               </div>
               <div class='profile-projects tab'>
 
